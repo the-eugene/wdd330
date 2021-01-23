f7ba8f4e4089ea00b8c3e922ece1b52cea655efa
the-eugene <72322861+the-eugene@users.noreply.github.com>
2021-01-23 14:33:18 -0600

    week3 notes

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,8 @@
 function init(){
 const links = [
     {label: "Week1 Notes", url: "week1/index.html"},
-    {label: "Week2 Notes", url: "week2/index.html"}
+    {label: "Week2 Notes", url: "week2/index.html"},
+    {label: "Week3 Notes", url: "week3/index.html"}
   ];
 
 var contents=document.getElementById("Contents");
