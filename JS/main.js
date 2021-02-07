1ee90edc378161181630743fe322c06cd09f87d0
the-eugene <72322861+the-eugene@users.noreply.github.com>
2021-02-06 18:40:57 -0600

    week5

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,8 @@ const links = [
     {label: "Week1 Notes", url: "week1/index.html"},
     {label: "Week2 Notes", url: "week2/index.html"},
     {label: "Week3 Notes", url: "week3/index.html"},
-    {label: "Week3 Notes", url: "week4/index.html"}
+    {label: "Week4 Notes", url: "week4/index.html"},
+    {label: "Week5 Notes", url: "week5/index.html"}
   ];
 
 var contents=document.getElementById("Contents");
