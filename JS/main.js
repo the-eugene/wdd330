efb05b5c1fd2a44899aad86ad769cb9c60c55aec
the-eugene <72322861+the-eugene@users.noreply.github.com>
2021-02-25 23:05:52 -0600

    week 8

--- JS/main.js
+++ JS/main.js
@@ -5,11 +5,12 @@ const links = [
     {label: "Week3 Notes", url: "week3/index.html"},
     {label: "Week4 Notes", url: "week4/index.html"},
     {label: "Week5 Notes", url: "week5/index.html"},
-    {label: "Week6 - Challenge 1", url: "challenge1/index.html"},
+    {label: "Week6 - Challenge 1", url: "challenge1/index.html"}
   ];
 
   const links2 = [
     {label: "Week7 Notes", url: "week7/index.html"},
+    {label: "Week8 Notes", url: "week8/index.html"}
   ];
 
 
